d01fd44534cc70ec962236deca92534f1d5467f1
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-01-24 13:11:55 +0100

    Add rule in ALB SG to accept requests from VPN on Port 80

--- 02-sg/alb.tf
+++ 02-sg/alb.tf
@@ -7,3 +7,13 @@ module "app_alb" {
   sg_name        = "app-alb"
   # sg_ingress_rules = var.mongodb_sg_ingress_rules
 }
+
+# App ALB should accept connections only from VPN since its internal
+resource "aws_security_group_rule" "app_alb_vpn" {
+  source_security_group_id = module.app_alb.sg_id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = module.vpn.sg_id
+}
