57cf230addc5219121b87cafb28889bddcf7b429
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-01-24 20:10:23 +0100

    Complete other steps in the autoscaling

--- 06-catalogue/catalogue.tf
+++ 06-catalogue/catalogue.tf
@@ -13,7 +13,7 @@ module "catalogue" {
   name                                = "${local.name}-${var.tags.Component}-ami"
   instance_type                       = "t3.micro"
   ami                                 = data.aws_ami.centos.id
-  vpc_security_group_ids              = [data.aws_ssm_parameter.mongodb_sg_id.value]
+  vpc_security_group_ids              = [data.aws_ssm_parameter.catalogue_sg_id.value]
   subnet_id                           = local.private_subnet_id
   create_spot_instance                = true
   spot_type                           = "persistent"
@@ -55,5 +55,54 @@ resource "null_resource" "catalogue" {
       "sudo chmod +x /tmp/bootstrap.sh",
       "sudo sh /tmp/bootstrap.sh catalogue dev"
     ]
+  }
+}
+
+# 3. Stop the instance
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = module.catalogue.id
+  state       = "stopped"
+  depends_on  = [null_resource.catalogue]
+}
+
+# 4. Take AMI
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${local.name}-${var.tags.Component}-${local.current_time}"
+  source_instance_id = module.catalogue.id
+}
+
+# 5. Delete the instance
+resource "null_resource" "catalogue_delete" {
+  triggers = {
+    instance_id = module.catalogue.id
+  }
+
+  provisioner "local-exec" {
+    command = [
+      "aws ec2 cancel-spot-instance-requests --spot-instance-request-ids ${module.catalogue.spot_instance_id}",
+      "aws ec2 terminate-instances --instance-ids ${module.catalogue.id}"
+    ]
+  }
+
+  depends_on = [aws_ami_from_instance.catalogue]
+}
+
+# 6. Create launch template with AMI
+resource "aws_launch_template" "catalogue" {
+  name = "${local.name}-${var.tags.Component}"
+
+  image_id                             = aws_ami_from_instance.catalogue.id
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type                        = "t2.micro"
+  update_default_version               = true
+
+  vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "${local.name}-${var.tags.Component}"
+    }
   }
 }
