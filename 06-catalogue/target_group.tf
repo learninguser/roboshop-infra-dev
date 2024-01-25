05f5345b8f39181301ac5668fb36a13168fe5224
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-01-25 22:28:30 +0100

    Set instance deregistration delay to 60 sec

--- 06-catalogue/target_group.tf
+++ 06-catalogue/target_group.tf
@@ -1,16 +1,17 @@
 resource "aws_lb_target_group" "catalogue" {
-  name     = "${local.name}-${var.tags.Component}"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = data.aws_ssm_parameter.vpc_id.value
+  name                 = "${local.name}-${var.tags.Component}"
+  port                 = 8080
+  protocol             = "HTTP"
+  vpc_id               = data.aws_ssm_parameter.vpc_id.value
+  deregistration_delay = 60
 
   health_check {
-    path = "/health"
-    port = 8080
-    healthy_threshold = 2
+    path                = "/health"
+    port                = 8080
+    healthy_threshold   = 2
     unhealthy_threshold = 3
-    timeout = 5
-    interval = 10
-    matcher = "200-299"  # has to be HTTP 200-299 or fail
+    timeout             = 5
+    interval            = 10
+    matcher             = "200-299" # has to be HTTP 200-299 or fail
   }
 }
