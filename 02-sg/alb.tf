111eb78ca7d93e8cb05a0e009d0fb98e35ff8f9c
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-01-25 22:36:39 +0100

    Create SG for web ALB  and add rule to accept traffic on 443

--- 02-sg/alb.tf
+++ 02-sg/alb.tf
@@ -17,3 +17,24 @@ resource "aws_security_group_rule" "app_alb_vpn" {
   protocol                 = "tcp"
   security_group_id        = module.vpn.sg_id
 }
+
+# Web ALB
+module "web_alb" {
+  source         = "../../terraform-aws-sg"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_description = "SG for ALB"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+  sg_name        = "web-alb"
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
+}
+
+# web ALB should accept connections from anywhere
+resource "aws_security_group_rule" "web_alb_internet" {
+  cidr_blocks       = ["0.0.0.0/0"]
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = module.web.sg_id
+}
