935f4c33e362c9415394efb5bc31a27888bf4f22
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-01-25 22:32:47 +0100

    Change Autoscaling group name in autoscaling policy

--- 06-catalogue/catalogue.tf
+++ 06-catalogue/catalogue.tf
@@ -159,7 +159,7 @@ resource "aws_lb_listener_rule" "catalogue" {
 
 # 9. Add autoscaling policy
 resource "aws_autoscaling_policy" "catalogue" {
-  autoscaling_group_name = "${local.name}-${var.tags.Component}"
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
   name                   = "${local.name}-${var.tags.Component}"
   policy_type            = "TargetTrackingScaling"
 
