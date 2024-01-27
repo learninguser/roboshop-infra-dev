58030d48e3b12fa3e7185f06c610ed00deb14c2a
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-01-27 16:42:37 +0100

    Create Record for web-alb

---
+++ 08-web-alb/records.tf
@@ -0,0 +1,17 @@
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = var.zone_name
+
+  records = [
+    {
+      name    = "web-${var.environment}"
+      type    = "A"
+      alias   = {
+        name    = aws_lb.web_alb.dns_name
+        zone_id = aws_lb.web_alb.zone_id
+      }
+    }
+  ]
+}
