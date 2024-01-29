1b10c2e2e923977be7803ea35af5d0214e2ffbc5
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-01-29 14:11:15 +0100

    Chnage bucket for Web ALB

--- 08-web-alb/provider.tf
+++ 08-web-alb/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket         = "learninguser"
-    key            = "web-dev"
+    key            = "web-alb-dev"
     region         = "us-east-1"
     dynamodb_table = "terraform_lock"
   }
