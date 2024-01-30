22ac0c2495622ef81c3d15ecc226fda187d687a8
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-01-30 21:28:18 +0100

    Export Web ALB DNS name to SSM

--- 08-web-alb/parameters.tf
+++ 08-web-alb/parameters.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "web_alb_listener_arn" {
     type = "String"
     value = aws_lb_listener.web_alb.arn
 }
+
+resource "aws_ssm_parameter" "web_alb_dns_name" {
+    name = "/${var.project_name}/${var.environment}/web_alb_dns_name"
+    type = "String"
+    value = aws_lb.web_alb.dns_name
+}
