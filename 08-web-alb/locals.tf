8296204aa57a72d6f10e863d38be0d0b639520cf
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-02-03 16:54:44 +0100

    Remove unwanted code

--- 08-web-alb/locals.tf
+++ 08-web-alb/locals.tf
@@ -1,5 +1,3 @@
 locals{
     name = "${var.project_name}-${var.environment}"
-    public_subnet_id = element(split(",", data.aws_ssm_parameter.public_subnet_ids.value), 0)
-    current_time = formatdate("YYYY-MM-DD-hh-mm", timestamp())
 }
