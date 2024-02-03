ade789ee9bf89104120ee722996e2a5601fb087d
Pavan Kumar Akula <akulapavankumar@outlook.com>
2024-02-03 17:13:16 +0100

    Allow port 80 from internet

--- 02-sg/rules.tf
+++ 02-sg/rules.tf
@@ -103,11 +103,11 @@ resource "aws_security_group_rule" "payment_app_alb" {
   security_group_id        = module.payment.sg_id
 }
 
-resource "aws_security_group_rule" "web_app_alb" {
-  source_security_group_id = module.app_alb.sg_id
-  type                     = "ingress"
-  from_port                = 8080
-  to_port                  = 8080
-  protocol                 = "tcp"
-  security_group_id        = module.web.sg_id
+resource "aws_security_group_rule" "web_internet" {
+  cidr_blocks       = ["0.0.0.0/0"]
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = module.web.sg_id
 }
